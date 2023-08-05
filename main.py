2a60cffe0bede417b0525c83f456e3f884172b92
mahmud <makhmoodsalimov007@gmail.com>
2023-08-05 14:59:59 +0500

    345

---
+++ main.py
@@ -0,0 +1,114 @@
+# print('welcome to phyton')
+# print()
+# print()
+
+# first_name1 = 'Makhmood'
+# FirstName = 'Ivan'
+# print(FirstName)
+
+
+# first_name = ' Jojo'
+# print(type(first_name))
+# last_name = "Oyti"
+# print(type(last_name))
+# age = '12'
+# print(type(age))
+# print(first_name + ' ' + last_name + ' ' + str(age))
+# print(first_name,last_name,str(age))
+# print(f'Имя {first_name} Фамилия {last_name} Возраст {age}')
+
+
+# sen = "i don't speak"
+# sen2 = 'i don"t speak'
+# sen3 = 'i don\'t speak'
+# print(sen)
+# print(sen2)
+# print(sen3)
+#
+# ''' много строчные комментарии'''
+#
+# string1 = """ I love
+#        Python
+#              and
+#                 Linux"""
+#
+# print(string1)
+# print('i love\n\t   Python\n\t\t\t  mazafaka')
+
+
+# num1 = 12
+# num2 = 13
+# print(num1+num2)
+# print(f' {num1}+{num2}={num1+num2}')
+#
+# number = 5+2
+# print(number)
+#
+# number = 60 * 23
+# print(number)
+
+
+# a = 2
+# b = 3
+# c = a ** b
+# print(c)
+#
+#
+# z = 16 ** 0.5
+# print(z)
+#
+#
+# v = 10 / 5
+# print(v)
+# print(type(v))
+#
+#
+# a = 3.0
+# print(int(a))
+#
+
+
+# num1 = 10 % 3
+# print(num1)
+#
+# num2 = 10 / 3
+# print(int(num2))
+#
+#
+# num3 = 10 // 3
+# print(num3)
+
+
+#
+# cnt = 5
+# cnt = cnt+1
+# print(cnt)
+
+
+#
+# cnt = 5
+# cnt +=1
+# print(cnt)
+
+
+#
+#
+# cnt = 10
+# cnt *=3
+# print(cnt)
+
+
+# raise ZeroDivisionError('Sap rais')
+
+# num1 = 12
+# num2 = 13
+# print(num1+num2)
+#
+#
+#
+# num1 = int(input('Enter your number: '))
+# print(int)
+# num2 = int(input('Enter your home number: '))
+# print(int)
+# print(num1, num2)
+#
